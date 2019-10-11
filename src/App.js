26fdf6ba07501c80443f999e8061b55148dd66b9
Mounika Gollu <mounika.gollu@mountblue.io>
2019-10-11 13:51:46 +0530

    Added registration and login pages by Mounika

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Register from './components/registration'
+import Login from './components/login'
 
 class App extends Component {
   render() {
@@ -9,6 +11,8 @@ class App extends Component {
           <h2> Learn365</h2>
         </div>
         
+      <Login />
+        {/* <Register /> */}
        
       </div>
     );
---
+++ src/components/login.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render () {
+        return (
+            <div>
+            <div className="App__Form">
+                <div className="FormTitle">
+                    <h3>Sign In</h3>
+                    {/* <a href="#" className="FormTitle__Link">Sign In</a> */}
+                </div>
+                <div className="FormCenter">
+                    <form className="FormFields" onSubmit={this.handleSubmit}>
+                    <div className="FormField">
+                            <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
+                        </div>
+
+                        <div className="FormField">
+                            <label className="FormField__Label" htmlFor="password">Password</label>
+                            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+                        </div>
+
+                        <div className="FormField">
+                            <button className="FormField__Button">Sign In</button> 
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>  
+        </div>
+            
+        
+        )
+    }
+}
+export default Login;
