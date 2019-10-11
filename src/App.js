e7d79418df31e9b7d348ac6ae543d6040028884c
Pawan Kumar <pawan.kumar10@mountblue.io>
2019-10-11 13:58:59 +0530

    initial commite

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import firebase from 'firebase';
+import styledFirebaseAuth from 'react-firebase'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2> Learn365</h2>
+         
         </div>
         
        
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
