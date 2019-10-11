c8d68a49feb62437f0677f7d5d4b983b0e0a4a0e
Mounika Gollu <mounika.gollu@mountblue.io>
2019-10-11 17:28:07 +0530

    commit it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import { BrowseRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Register from './components/registration'
-import Login from './components/login'
+// import Login from './components/login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2> Learn365</h2>
-        </div>
-        
-      <Login />
-        {/* <Register /> */}
-       
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <h2> Learn365</h2>
+            <p>Agile way of learning</p>
+          </div>
+      {/* <Login /> */}
+      {/* <Register /> */}
       </div>
+      </Router> 
     );
   }
 }
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
+// import  Route  from 'react-router-dom';
 class Register extends Component {
     render () {
         return (
+            
             <div>
             <div className="App__Form">
                 <div className="FormTitle">
                     <h3>SignUp</h3>
-                    {/* <a href="#" className="FormTitle__Link">Sign In</a> or <a href="#"
+                    {/* < href="#" className="FormTitle__Link">Sign In</a> or <a href="#"
                     className="FormTitle__Link FormTitle__Link--Active">Sign Up</a> */}
                 </div>
-                <div className="FormCenter">
-                    <form className="FormFields" onSubmit={this.handleSubmit}>
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="name">Full Name</label>
-                            <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
-                        </div>
+                
+                    <div className="FormCenter">
+                        <form className="FormFields" onSubmit={this.handleSubmit}>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="name">Full Name</label>
+                                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+                            </div>
 
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="password">Password</label>
-                            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
-                        </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="password">Password</label>
+                                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+                            </div>
 
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
-                        </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
+                            </div>
 
-                        {/* <div className="FormField">
-                            <label className="FormField__CheckboxLabel">
-                            <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"/>I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                            </label> 
-                        </div> */}
+                            {/* <div className="FormField">
+                                <label className="FormField__CheckboxLabel">
+                                <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"/>I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
+                                </label> 
+                            </div> */}
 
-                        <div className="FormField">
-                            <button className="FormField__Button mr-20">Sign Up</button> <a href="#"
-                            className="FormField__Link">I'm already member</a>
-                        </div>
+                            <div className="FormField">
+                                <button className="FormField__Button mr-20">Sign Up</button> <a href="#"
+                                className="FormField__Link">I'm already member</a>
+                            </div>
 
-                    </form>
-                </div>
-
+                        </form>
+                    </div>  
             </div>  
         </div>
             
